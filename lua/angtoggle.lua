d54eaf7bb030ac677d3513c3f527f10e8bef6872
BitInByte <bitinbyte@outlook.com>
2022-05-19 15:18:37 +0100

    testing switch table

--- lua/angtoggle.lua
+++ lua/angtoggle.lua
@@ -1,5 +1,24 @@
+local function toggle()
+  print("About to toggle")
+end
+
 local function load_command(args)
   print(args)
+
+  -- creating table to act as a switch
+  local func_table = {
+    toggle = toggle
+  }
+
+  local func = func_table[args]
+
+  if (func) then
+    func()
+  else
+    -- echohl Error
+    print("Sorry this plugin only works with versions of neovim that support lua")
+    return
+  end
 end
 
 return {
